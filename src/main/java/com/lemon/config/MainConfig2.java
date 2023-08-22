0324624592f8010c5275473b07ce03f834e12778
lemonliu1993 <2305247091@qq.com>
2023-08-23 06:49:24 +0800

    @Scope设置组件作用域

---
+++ src/main/java/com/lemon/config/MainConfig2.java
@@ -0,0 +1,32 @@
+package com.lemon.config;
+
+import com.lemon.bean.Person;
+import org.springframework.context.annotation.*;
+
+import static org.springframework.beans.factory.config.BeanDefinition.SCOPE_PROTOTYPE;
+
+/**
+ * Created by lemoon on 2023/8/22 08:14
+ */
+//配置类==配置文件
+@Configuration  //告诉Spring这是一个配置类
+public class MainConfig2 {
+
+    //默认是单例的
+
+    /**
+     * SCOPE_PROTOTYPE :多实例的,ioc容器启动并不会去调用方法创建对象放在容器中
+     * 每次获取的时候才会调用方法创建对象
+     * SCOPE_SINGLETON :单实例的(默认值),ioc容器启动会调用方法创建对象放到ioc容器中。
+     * 以后每次获取就是直接从容器(map.get())中获取
+     * SCOPE_REQUEST :同一次请求创建一个实例
+     * SCOPE_SESSION :同一个session创建一个实例
+     *
+     * @return
+     */
+    @Scope(value = SCOPE_PROTOTYPE)
+    @Bean("person")
+    public Person person() {
+        return new Person("lisi", 18);
+    }
+}
--- src/test/java/com/lemon/test/IOCTest.java
+++ src/test/java/com/lemon/test/IOCTest.java
@@ -1,6 +1,7 @@
 package com.lemon.test;
 
 import com.lemon.config.MainConfig;
+import com.lemon.config.MainConfig2;
 import org.junit.Test;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
@@ -8,6 +9,20 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
  * Created by lemoon on 2023/8/22 22:27
  */
 public class IOCTest {
+
+
+
+    @Test
+    public void test02(){
+        AnnotationConfigApplicationContext applicationContext = new AnnotationConfigApplicationContext(MainConfig2.class);
+        String[] definitionNames = applicationContext.getBeanDefinitionNames();
+        for (String name : definitionNames) {
+            System.out.println(name);
+        }
+        Object bean = applicationContext.getBean("person");
+        Object bean2 = applicationContext.getBean("person");
+        System.out.println(bean == bean2);
+    }
 
     @Test
     public void test01(){
