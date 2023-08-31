5e8794f068151cfd1868d56374b968a86982d6f1
lemonliu1993 <2305247091@qq.com>
2023-08-31 23:26:16 +0800

    @PropertySource加载外部配置文件

--- src/main/java/com/lemon/bean/Person.java
+++ src/main/java/com/lemon/bean/Person.java
@@ -10,13 +10,16 @@ public class Person {
     //使用@Value赋值
     //1.基本数值
     //2.可以写SpEL:#{}
-    //3.可以写${},取出配置文件中的值(在运行环境变量里面的值)
+    //3.可以写${},取出配置文件【properties】中的值(在运行环境变量里面的值)
 
 
     @Value("张三")
     private String name;
     @Value("#{20-2}")
     private int age;
+
+    @Value("${person.nickName}")
+    private String nickName;
 
     public String getName() {
         return name;
@@ -34,12 +37,12 @@ public class Person {
         this.age = age;
     }
 
-    @Override
-    public String toString() {
-        return "Person{" +
-                "name='" + name + '\'' +
-                ", age=" + age +
-                '}';
+    public String getNickName() {
+        return nickName;
+    }
+
+    public void setNickName(String nickName) {
+        this.nickName = nickName;
     }
 
     public Person(String name, int age) {
@@ -48,5 +51,14 @@ public class Person {
     }
 
     public Person() {
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                ", nickName='" + nickName + '\'' +
+                '}';
     }
 }
--- src/test/java/com/lemon/test/IOCTest_PropertyValue.java
+++ src/test/java/com/lemon/test/IOCTest_PropertyValue.java
@@ -5,6 +5,7 @@ import com.lemon.config.MainConfigOfLifeCycle;
 import com.lemon.config.MainConfigOfPropertyValues;
 import org.junit.Test;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.core.env.ConfigurableEnvironment;
 
 /**
  * Created by lemoon on 2023/8/26 10:36
@@ -21,6 +22,11 @@ public class IOCTest_PropertyValue {
 
         Person person = applicationContext.getBean(Person.class);
         System.out.println(person);
+
+        //配置文件中的值都会加载到环境变量中
+        ConfigurableEnvironment environment = applicationContext.getEnvironment();
+        String property = environment.getProperty("person.nickName");
+        System.out.println(property);
         applicationContext.close();
     }
 
