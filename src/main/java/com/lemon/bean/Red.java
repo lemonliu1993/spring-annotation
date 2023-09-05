ab591345d878b754619da0d1e45ea2041134df68
lemonliu1993 <2305247091@qq.com>
2023-09-05 08:25:37 +0800

    Aware注入Spring底层组件

--- src/main/java/com/lemon/bean/Red.java
+++ src/main/java/com/lemon/bean/Red.java
@@ -1,7 +1,33 @@
 package com.lemon.bean;
 
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.BeanNameAware;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+import org.springframework.context.EmbeddedValueResolverAware;
+import org.springframework.stereotype.Component;
+import org.springframework.util.StringValueResolver;
+
 /**
  * Created by lemoon on 2023/8/25 22:09
  */
-public class Red {
+@Component
+public class Red implements ApplicationContextAware,BeanNameAware,EmbeddedValueResolverAware{
+
+    private ApplicationContext applicationContext;
+
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+        System.out.println("传入的ioc:" + applicationContext);
+        this.applicationContext = applicationContext;
+    }
+
+
+    public void setBeanName(String name) {
+        System.out.println("当前bean的名字："+ name);
+    }
+
+    public void setEmbeddedValueResolver(StringValueResolver resolver) {
+        String resolveStringValue = resolver.resolveStringValue("你好${os.name} 我是#{20*100}");
+        System.out.println("解析的字符串:"+ resolveStringValue);
+    }
 }
